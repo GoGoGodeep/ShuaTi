d297081891724c84f43778f8f48fe0750f5a55eb
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-03-25 11:09:23 +0800

    Update 移除元素.py

--- 移除元素.py
+++ 移除元素.py
@@ -2,7 +2,8 @@ def removeElement(self, nums: List[int], val: int) -> int:
     # 快慢指针法
     fast = slow = 0
     
-    while fast < len(nums):
+    while fast < len(nums):     # 不加等于是因为，a = size 时，nums[a] 会越界
+        # slow 用来收集不等于 val 的值，如果 fast 对应值不等于 val，则把它与 slow 替换
         if nums[fast] != val:
             nums[slow] = nums[fast]
             slow += 1
