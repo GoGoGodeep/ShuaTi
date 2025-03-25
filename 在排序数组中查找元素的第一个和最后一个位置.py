1f3aa80bec67a224982f3d95ff3695f15ae4b7e8
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-03-25 09:51:34 +0800

    Update 在排序数组中查找元素的第一个和最后一个位置.py

--- 在排序数组中查找元素的第一个和最后一个位置.py
+++ 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,1 +1,34 @@
+# 还是leetcode的写法简单
 
+def getleft():
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] >= target:
+            right = mid - 1
+        else:
+            left = mid + 1
+        return left
+
+
+def getright():
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] <= target:
+            left = mid + 1
+        else:
+            right = mid - 1
+        return right
+
+left = getleft()
+right = getright()
+
+if left <= right:
+    return [left, right]
+else:
+    return [-1, -1]
