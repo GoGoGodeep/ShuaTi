7eaa62a6bcbf78d5a61af7bc020d160874daa278
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-03-26 10:35:28 +0800

    快慢指针

--- 数组/移动零.py
+++ 数组/移动零.py
@@ -2,7 +2,7 @@ def moveZeroes(self, nums: List[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
-    slow = fast = 0
+    slow = fast = 0 
 
     while fast < len(nums):
         if nums[fast] != 0:
