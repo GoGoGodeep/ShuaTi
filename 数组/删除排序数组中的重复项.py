33fac1cb87398422f02b76f5ace91c3e85dbf6f7
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-03-26 10:33:59 +0800

    Create 删除排序数组中的重复项.py

---
+++ 数组/删除排序数组中的重复项.py
@@ -0,0 +1,10 @@
+def removeDuplicates(self, nums: List[int]) -> int:
+    slow = fast = 0
+
+    while fast < len(nums):
+        if nums[slow] != nums[fast]:
+            slow += 1
+            nums[slow] = nums[fast]
+        fast += 1
+
+    return slow + 1
