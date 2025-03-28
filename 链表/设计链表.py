4132512d46af4c12927e828a0fe41fa5f79e1a3f
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-03-28 14:11:13 +0800

    Create 设计链表.py

---
+++ 链表/设计链表.py
@@ -0,0 +1,69 @@
+"""
+示例：
+
+输入
+["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+[[], [1], [3], [1, 2], [1], [1], [1]]
+输出
+[null, null, null, null, 2, null, 3]
+
+解释
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // 链表变为 1->2->3
+myLinkedList.get(1);              // 返回 2
+myLinkedList.deleteAtIndex(1);    // 现在，链表变为 1->3
+myLinkedList.get(1);              // 返回 3
+"""
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = ListNode(0)
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+
+        curr = self.head
+        for _ in range(index + 1):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.size:
+            return 
+        
+        index = max(0, index)
+        
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        temp = ListNode(val)
+        temp.next = curr.next
+        curr.next = temp
+    
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        
+        self.size -= 1
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        curr.next = curr.next.next
