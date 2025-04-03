50be2346e241b198b9c9ddc5d14a8c0dc7afba32
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-03 14:19:22 +0800

    Update 移除元素.py

--- 数组/移除元素.py
+++ 数组/移除元素.py
@@ -1,5 +1,5 @@
+# 快慢指针法 
 def removeElement(self, nums: List[int], val: int) -> int:
-    # 快慢指针法 
     fast = slow = 0
     
     while fast < len(nums):     # 不加等于是因为，a = size 时，nums[a] 会越界
@@ -10,3 +10,22 @@ def removeElement(self, nums: List[int], val: int) -> int:
         fast += 1
     
     return slow
+
+# 双指针法
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            while left <= right and nums[left] != val:
+                left += 1
+            
+            while left <= right and nums[right] == val:
+                right -= 1
+            
+            if left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        
+        return left
