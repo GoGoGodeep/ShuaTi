0a90990425bcabc48bee11ed1906ad027b2e74fd
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-11 21:15:50 +0800

    递归、前序

--- 二叉树/合并二叉树.py
+++ 二叉树/合并二叉树.py
@@ -4,7 +4,7 @@ class Solution:
             return root2
         if not root2:
             return root1
-        
+         
         root1.val += root2.val
 
         root1.left = self.mergeTrees(root1.left, root2.left)
