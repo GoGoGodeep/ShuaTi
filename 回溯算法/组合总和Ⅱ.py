be89ae781decece3049eb9958646b2c2405499a7
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-20 10:01:28 +0800

    回溯

--- 回溯算法/组合总和Ⅱ.py
+++ 回溯算法/组合总和Ⅱ.py
@@ -10,6 +10,17 @@ class Solution:
                 return
 
             for i in range(start, len(candidates)):
+                # 保证不出现重复数字
+                """
+                候选数组包含重复数字
+                例如：candidates = [1,1,2,5]，target = 8
+                排序后仍然是 [1,1,2,5]
+                如果不跳过重复的 1，回溯过程可能会选择两次 1（来自不同的位置），导致生成 [1,2,5] 两次：
+                
+                第一次选 candidates[0]（第一个 1），然后选 2 和 5
+                第二次选 candidates[1]（第二个 1），然后选 2 和 5
+                这样结果里会有两个 [1,2,5]，但实际上它们是相同的组合。
+                """
                 if i > start and candidates[i] == candidates[i-1]:
                     continue
 
