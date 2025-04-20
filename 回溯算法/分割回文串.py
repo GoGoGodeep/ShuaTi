eb6a5b60b602a2991e8d90eb403f0b0e03c870de
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-20 10:42:12 +0800

    回溯

--- 回溯算法/分割回文串.py
+++ 回溯算法/分割回文串.py
@@ -2,17 +2,33 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         result = []
 
+        # # 优化
+        # def backtrack(start, path):
+        #     if start == len(s):
+        #         result.append(path)
+        #         return
+
+        #     for end in range(start+1, len(s)+1):
+        #         sub = s[start:end]
+
+        #         if sub == sub[::-1]:
+        #             backtrack(end, path + [sub])
+
+        # 未优化
+        def is_true(s):
+            return s == s[::-1]
 
         def backtrack(start, path):
             if start == len(s):
-                result.append(path)
-                return
-
+                result.append(path[:])
+            
             for end in range(start+1, len(s)+1):
                 sub = s[start:end]
 
-                if sub == sub[::-1]:
-                    backtrack(end, path + [sub])
-
+                if is_true(sub):
+                    path.append(sub)
+                    backtrack(end, path)
+                    path.pop()
+        
         backtrack(0, [])
         return result
