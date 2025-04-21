a8f8d337648d6bb4df6831757ab0a5ac11082db5
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-21 16:22:23 +0800

    回溯、分割回文串

--- 回溯算法/复原IP地址.py
+++ 回溯算法/复原IP地址.py
@@ -2,28 +2,27 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         result = []
 
-        def is_true(s, start, end):
+        def is_true(start, end):
             if s[start] == '0' and start != end:
                 return False
+            
             num = int(s[start:end+1])
             return 0 <= num <= 255
-
+        
         def backtrack(start, path):
             if start == len(s) and len(path) == 4:
                 result.append('.'.join(path))
                 return
-            
+
             if len(path) > 4:
                 return
-
+            
             for end in range(start, min(start + 3, len(s))):
-                if is_true(s, start, end):
+                if is_true(start, end):
                     sub = s[start:end+1]
                     path.append(sub)
                     backtrack(end+1, path)
                     path.pop()
-            
+        
         backtrack(0, [])
-
         return result
-            
