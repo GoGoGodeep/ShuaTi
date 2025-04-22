98945daff8174060386dc28d5ac268a26709b27f
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-22 10:47:55 +0800

    回溯

--- 回溯算法/全排序Ⅱ.py
+++ 回溯算法/全排序Ⅱ.py
@@ -12,6 +12,7 @@ class Solution:
             for i in range(len(nums)):
                 if used[i]:
                     continue
+                # 剪枝条件：当前数字与前一个数字相同，且前一个数字未被使用过
                 if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
                     continue
 
