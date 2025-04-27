6910bd3c0ced9c4a694c6ba45820f8338f42f5cb
Saki <73456432+GoGoGodeep@users.noreply.github.com>
2025-04-27 17:30:47 +0800

    贪心

--- 贪心算法/跳跃游戏.py
+++ 贪心算法/跳跃游戏.py
@@ -1,14 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         curr = 0
-        i = 0
 
-        while i <= curr:
-            curr = max(curr, i + nums[i])
+        for i in range(len(nums)):
+            if i <= curr:
+                curr = max(curr, i + nums[i])
 
-            if curr >= len(nums) - 1:
-                return True
+                if curr >= len(nums) - 1:
+                    return True
             
-            i += 1
-
-        return False
+            else:       # 如果当前的索引大于了curr，说明已无法到达索引的位置
+                return False
